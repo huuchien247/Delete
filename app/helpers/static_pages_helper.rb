2194b80432c2e97759b757d9226b91273e5c37c7
Nguyen Huu Chien <chien.nguyen.fw@gmail.com>
2020-06-22 10:49:24 +0700

    Finish Chapter 3, 4, 5

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,7 @@
 module StaticPagesHelper
+	def full_title page_title = ""
+		base_title = "Ruby on Rails Tutorial Sample App"
+		page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+		
+	end
 end
